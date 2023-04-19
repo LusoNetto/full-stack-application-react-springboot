f047e1fa7cebf3be3108bcb3034f83fe246a9ade
LusoNetto <luciano.silva137@hotmail.com>
2023-04-19 16:55:10 -0300

    first commit up the project

---
+++ src/main/java/com/fullstackapplicationreactspringboot/fullstackapplicationreactspringboot/FullStackApplicationReactSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.fullstackapplicationreactspringboot.fullstackapplicationreactspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullStackApplicationReactSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FullStackApplicationReactSpringbootApplication.class, args);
+	}
+
+}
